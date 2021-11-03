8a2e60e5b3533ffb39fd85ddce19e881ff1f5b72
Stepan Kolodkin <zorokon@yandex.ru>
2021-11-03 14:00:52 +0300

    finalized the draft version

--- main.py
+++ main.py
@@ -1,18 +1,50 @@
 import requests
 import json
 import time
-from pprint import pprint
 
 import const
 
 
+def answer_user_bot(data):
+    data = {
+        'chat_id': const.MY_ID,
+        'text': data
+    }
+    url = const.URL.format(
+        token=const.TOKEN,
+        method=const.SEND_METH
+    )
+    response = requests.post(url, data=data)
+    print(response)
+
+
+def parse_weather_data(data):
+    for elem in data['weather']:
+        weather_state = elem['main']
+    temp = round(data['main']['temp'] - 273.15, 2)
+    city = data['name']
+    msg = f'The weather in {city}: Temp is {temp}, State is {weather_state}'
+    return msg
+
+
+def get_weather(location):
+    url = const.WEATHER_URL.format(city=location,
+                                   token=const.WEATHER_TOKEN)
+    response = requests.get(url)
+    if response.status_code != 200:
+        return 'city not found'
+    data = json.loads(response.content)
+    return parse_weather_data(data)
+
+
 def get_message(data):
-    pass
+    return data['message']['text']
 
 
 def save_update_id(update):
-    with open('update_id', 'w') as file:
+    with open(const.UPDATE_ID_FILE_PATH, 'w') as file:
         file.write(str(update['update_id']))
+    const.UPDATE_ID = update['update_id']
     return True
 
 
@@ -31,12 +63,12 @@ def main():
                 break
 
         if const.UPDATE_ID != needed_part['update_id']:
-            get_message(needed_part)
+            message = get_message(needed_part)
+            msg = get_weather(message)
+            answer_user_bot(msg)
             save_update_id(needed_part)
 
-        pprint(needed_part)
-        break
-        # time.sleep(2)
+        time.sleep(1)
 
 
 if __name__ == "__main__":
